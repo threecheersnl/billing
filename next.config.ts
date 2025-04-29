e9ca7e2c41c08fd13415e15541e48bd06fe3444f
threecheersnl <threecheers.nl@gmail.com>
2025-04-30 00:00:39 +0200

    Create nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: isProd ? '/billing' : '',
+  basePath: isProd ? '/billing' : '',
 };
 
 export default nextConfig;
